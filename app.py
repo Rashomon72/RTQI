18ac48ccfc1fb7459ae9a5fdc6140c0511890e94
Adamkoda2306 <147936412+Adamkoda2306@users.noreply.github.com>
2025-05-28 13:02:24 +0530

    modified according to gunicorn

--- app.py
+++ app.py
@@ -324,6 +324,7 @@ def upload_file():
 def health_check():
     return jsonify({"status": "ok"}), 200
 
-# if __name__ == '__main__':
-#     port = int(os.environ.get("PORT", 3000))  # Use Render's PORT or default to 3000 locally
-#     app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 3000))  # Use Render's PORT or default to 3000 locally
+    # app.run(host="0.0.0.0", port=port)
+    app.run()
